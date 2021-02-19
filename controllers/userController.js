3d5655e8fed41c5dcbe2155dc836a9334df4d42b
Arrasyidff <aff.anton20@gmail.com>
2021-02-20 03:56:27 +0800

    revisi release 1

--- controllers/userController.js
+++ controllers/userController.js
@@ -33,17 +33,17 @@ class UserController {
             }
         })
             .then(({data}) => {
-                // let j = 0
                 let results = []
                 let sample = ['GOJEK', 'GrabExpress', 'JNE', 'SiCepat', 'Paxel', 'AnterAja', 'Deliveree']
                 for (let i = 0; i < sample.length; i++) {
                     for (let j = 0; j < data.results.length; j++) {
                         if ( sample[i] === data.results[j].name) {
-                            results.push(data.results[j].name)
+                            results.push(data.results[j])
                         }
                     }
                 }
                 res.status(200).json({results})
+
             })
             .catch(err => {
                 res.json(err)
